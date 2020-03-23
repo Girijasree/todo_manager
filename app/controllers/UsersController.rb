297ceda25603dcfb63fd3aad72e8b3b74a6cc1f3
Girijasree <girijasree1234@gmail.com>
2020-03-23 19:08:46 +0530

    Commiting after adding usermanagement, by Girijasree

---
+++ app/controllers/UsersController.rb
@@ -0,0 +1,36 @@
+# users_controller.rb
+class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
+      def index
+        render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+      end
+
+      def create
+        name = params[:name]
+        email = paramss[:email]
+        password = params[:password]
+        new_user = User.create!(
+           name: name,
+           email: email,
+           password: password,
+        )
+        response_text = "Hey, your new user is created with Id:#{new_user.id}"
+        render plain: response_text
+  
+      end
+
+      def show
+        id = params[:id]
+        user = User.find(id)
+        render plain: user.to_pleasant_string
+      end
+  
+      def login
+        email = params[:email]
+        password = params[:password]
+        user_exists = User.where(email: email, password: password).exists?
+        render plain: user_exists
+      end
+    end
+  
