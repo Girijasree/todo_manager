4c90de66f32ea47a1a4b1849b2fd413e5d31a55b
Girijasree <girijasree1234@gmail.com>
2020-05-09 15:29:57 +0530

    added sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get "todos", to: "todos#index"
-  #post "todos/create", to: "todos#create"
-  #get "todos/:id", to: "todos#show"
   get "/" => "home#index"
   resources :todos
   resources :users
   post "users/login", to: "users#login"
+  get "/signin" => "sessions#new", as: :new_session
+  post "/signin" => "sessions#create", as: :session
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
